752744f57ab94279944a6badb00402c6db043382
Marcelo Zapaia <zapaiamarce@gmail.com>
2021-05-06 20:47:17 -0300

    text

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+const cosasQueAprendimos = [
+  {
+    tema: "terminal",
+    class: "",
+  },
+  {
+    tema: "node",
+    class: "",
+  },
+  {
+    tema: "oop",
+    class: "",
+  },
+  {
+    tema: "typescript",
+    class: "",
+  },
+  {
+    tema: "css",
+    class: "",
+  },
+  {
+    tema: "dom",
+    class: "special",
+  },
+];
+
 function main() {}
 
 main();

2d718f2a89305fcd344b6dd55fffc5730c868851
Marcelo Zapaia <zapaiamarce@gmail.com>
2021-05-06 20:41:19 -0300

    base

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-function main() {
-  const newLiEl = document.createElement("li");
-  newLiEl.textContent = "Soy un li creado desde Javascript";
-
-  const listaEl = document.querySelector(".lista");
-  listaEl.appendChild(newLiEl);
-
-  setTimeout(function () {
-    newLiEl.textContent = "otro texto";
-  }, 3000);
-}
+function main() {}
 
 main();
